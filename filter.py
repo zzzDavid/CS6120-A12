52fc109ebc6a5afd37e9cc6e470148a0130dbb9b
Niansong Zhang <zhangns@mail2.sysu.edu.cn>
2022-04-15 22:23:21 -0400

    add filter

--- filter.py
+++ filter.py
@@ -4,6 +4,6 @@ import json
 if __name__ == "__main__":
     lines = ""
     for line in sys.stdin:
-        if not line.startswith("<"):
+        if not line.startswith(">"):
             lines += line
     print(lines)
